52ee99cdf5b1535a48f3ba947eeed33814ccc7ed
bugra <bugraa@live.com>
2013-07-27 18:47:22 -0400

    Added more functionality

--- bunch.py
+++ bunch.py
@@ -1,3 +1,10 @@
 class Bunch:
     def __init__(self, **kwds):
         self.__dict__.update(kwds)
+
+    def __getstate__(self):
+        return self
+
+    def __setstate__(self, state):
+        self.update(state)
+        self.__dict__ = self
